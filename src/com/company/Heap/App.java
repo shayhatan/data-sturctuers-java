3360f6abaafacac8cc33d0de30c312a9d7ce45b1
unknown <shayhatan7@gmail.com>
2022-02-17 17:40:06 +0200

    data-structures

--- src/com/company/Heap/App.java
+++
@@ -1,7 +0,0 @@
-package com.company.Heap;
-
-public class App {
-    public static void main(String[] args) {
-
-    }
-}
--- src/com/company/Heap/Heap.java
+++ src/com/company/Heap/Heap.java
@@ -7,88 +7,100 @@ package com.company.Heap;
 public class Heap {
 
     private Integer[] heap;
-    private int current_position = -1;
+    private int size;
 
     public Heap(int size) {
         this.heap = new Integer[size];
     }
 
-    public void insert(int item) {
-
+    public void insert(int data) {
         if (isFull()) {
-            throw new RuntimeException("Heap is full...");
+            throw new RuntimeException("Heap is full");
         }
 
-        heap[++current_position] = item;
-        fixUp(current_position);
+        heap[size] = data;
+
+        // check heap properties
+        fixUp(size++);
     }
 
     private void fixUp(int index) {
         int parent_index = (index - 1) / 2;
 
-        while (parent_index >= 0 && heap[parent_index] < heap[index]) {
+        if (index > 0 && heap[index] > heap[parent_index]) {
             swap(index, parent_index);
-            index = parent_index;
-            parent_index = (index - 1) / 2;
+            fixUp(parent_index);
         }
     }
 
-    private void swap(int index1, int index2) {
-        int temp = heap[index1];
-        heap[index1] = heap[index2];
-        heap[index2] = temp;
+    private void swap(int a, int b) {
+        int temp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = temp;
     }
 
-    // remove the root node
+    public int getMax() {
+        if (size == 0) {
+            throw new RuntimeException("Heap is empty");
+        }
+        return heap[0];
+    }
+
     public int pool() {
-        int result = heap[0];
-        heap[0] = heap[current_position--];
-        heap[current_position + 1] = null; // avoid obsolete references
-        fixDown(current_position);
-        return result;
+        int max = getMax();
+        swap(0, --size);
+
+        fixDown(0);
+
+        return max;
     }
 
+    private void fixDown(int index) {
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+        int largest = index;
+
+        if (left < size) {
+
+            if (heap[index] < heap[left]) {
+                largest = left;
+            }
+
+            if (right < size) {
+
+                if (heap[largest] < heap[right]) {
+                    largest = right;
+                }
+            }
+
+            if (index != largest) {
+                swap(index, largest);
+                fixDown(largest);
+            }
+        }
+    }
+
+    public boolean isFull() {
+        return size == heap.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    //heapsort is doing pool() operation for N times - O(N(logN))
     public void heapsort() {
-        for (int i = 0; i <= current_position; i++) {
-            int temp = this.heap[0];
-            System.out.print(temp + " ");
-            heap[0] = heap[current_position - i];
-            heap[current_position - i] = temp;
-            fixDown(current_position - i - 1);
+        int n = size;
+
+        for (int i = 0; i < n; ++i) {
+            int max = pool();
+            System.out.println(max + " ");
         }
     }
 
-    private void fixDown(int upto) {
-        int index = 0;
-        if (upto < 0) {
-            upto = current_position;
-        }
-        int left_child = 0;
-        int right_child;
-
-        while (index <= upto && left_child <= upto) {
-
-            left_child = 2 * index + 1;
-            right_child = 2 * index + 2;
-
-            int child_to_swap;
-
-            if (right_child > upto) {
-                child_to_swap = left_child;
-            } else {
-                child_to_swap = (heap[left_child] > heap[right_child]) ? left_child : right_child;
-            }
-
-            if (heap[index] < heap[child_to_swap]) {
-                swap(index, child_to_swap);
-                index = child_to_swap;
-            } else {
-                index = upto + 1;
-            }
-        }
+    public int getSize() {
+        return size;
     }
 
-    private boolean isFull() {
-        return this.current_position == this.heap.length;
-    }
 }
